fd46ead97e9be2e75e99c017f9f03ff716b988cc
pblgllgs <pbl.gllgs@gmail.com>
2022-10-11 18:13:33 -0300

    updates

---
+++ Dockerfile
@@ -0,0 +1,2 @@
+FROM nginx:1.23.1
+COPY ./dist  /usr/share/nginx/html
--- src/pages/AroundYou.jsx
+++ src/pages/AroundYou.jsx
@@ -1,5 +1,4 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 
 import { Error, Loader, SongCard } from '../components';
